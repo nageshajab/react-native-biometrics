bd6a57354eba6d47c7439c48113a33c6aa40661a
Nagesh Ajab <nageshajab@hotmail.com>
2025-07-27 19:26:58 +0530

    added bottom menus, login with password screen, login screen, validate with hardcoded username, password

---
+++ Screens/SettingsScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+interface SettingsScreenProps {
+  // Add props if needed
+}
+
+const SettingsScreen: React.FC<SettingsScreenProps> = () => {
+  return <View></View>;
+};
+
+export default SettingsScreen;
---
+++ Screens/TabNavigator.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./HomeScreen";
+import SettingsScreen from "./SettingsScreen";
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+};
+export default TabNavigator;
