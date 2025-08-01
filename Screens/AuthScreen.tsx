2d9a4b4afc9cf7bfacd7d8ba46e1a3d6d9714853
Nagesh Ajab <nageshajab@hotmail.com>
2025-08-01 20:05:38 +0530

    added comments

--- Screens/AuthScreen.tsx
+++ Screens/AuthScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; //used to manage state
 import {
   View,
   Text,
@@ -7,39 +7,46 @@ import {
   TextInput,
   Button,
   Alert,
-} from "react-native";
-import tw from "twrnc";
-import { Entypo } from "@expo/vector-icons";
-import * as LocalAuthentication from "expo-local-authentication";
-import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
+} from "react-native"; // Imports various UI components from React Native
+import tw from "twrnc"; //Imports the twrnc library, which is a CSS framework for React Native.
+
+import { Entypo } from "@expo/vector-icons"; //Entypo icon set from Expo's vector icons library
+import * as LocalAuthentication from "expo-local-authentication"; //Entypo icon set from Expo's vector icons library
+import { useNavigation } from "@react-navigation/native"; //provides navigation functionality
+import { StackNavigationProp } from "@react-navigation/stack"; //used to define the navigation prop type
 
 type RootStackParamList = {
   Auth: undefined;
   TabNavigator: undefined;
-};
+}; //Defines a type for the root stack param list, which includes two routes: Auth and TabNavigator.
 
 type AuthScreenNavigationProp = StackNavigationProp<RootStackParamList, "Auth">;
 
 const AuthScreen = () => {
-  const navigation = useNavigation<AuthScreenNavigationProp>();
-  const [modalVisible, setModalVisible] = useState(false);
+  const navigation = useNavigation<AuthScreenNavigationProp>(); //Uses the useNavigation hook to get the navigation prop, which is typed as AuthScreenNavigationProp.
+
+  const [modalVisible, setModalVisible] = useState(false); //for showing hiding modal of login/ pwd screen
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handlePasswordLogin = () => {
-    setModalVisible(true);
-  };
+  const handlePasswordLogin = () =>
+    //sets the modalVisible state to true
+    {
+      setModalVisible(true);
+    };
 
-  const handleLogin = () => {
-    if (username === "test" && password === "test") {
-      navigation.navigate("TabNavigator");
-      setModalVisible(false);
-    } else {
-      Alert.alert("Invalid username or password");
-    }
-  };
+  const handleLogin = () =>
+    //Defines a handler function handleLogin that checks the username and password, and navigates to the TabNavigator route if they are valid.
+    {
+      if (username === "test" && password === "test") {
+        navigation.navigate("TabNavigator");
+        setModalVisible(false);
+      } else {
+        Alert.alert("Invalid username or password");
+      }
+    };
 
+  //Defines a handler function handleBiometricAuth that uses the Expo Local Authentication library to authenticate the user biometrically.
   const handleBiometricAuth = async () => {
     const biometricAuth = await LocalAuthentication.authenticateAsync({
       promptMessage: "Login to app with biometrics",
